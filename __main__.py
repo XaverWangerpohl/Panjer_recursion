8efb7b7ce8723fcce50e329910bcf2a52d191978
XaverWangerpohl <71377493+XaverWangerpohl@users.noreply.github.com>
2021-02-05 17:37:07 +0100

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -20,9 +20,9 @@ def main():
     ws1 = wb['Main']
 
     """Number of claims distribution"""
-    n_o_c = Distribution(type=distr[ws1['E2'].value], value1=ws1['E3'].value, value2=ws1['E4'].value,trunc=bool(ws1['E6'].value))
+    n_o_c = Distribution(type=distr[ws1['E2'].value], value1=ws1['E3'].value, value2=ws1['E4'].value, trunc=ws1['E6'].value)
     """Amount per claim Distribution"""
-    a_p_c = Distribution(type=distr[ws1['F2'].value], value1=ws1['F3'].value, value2=ws1['F4'].value,trunc=bool(ws1['F6'].value))
+    a_p_c = Distribution(type=distr[ws1['F2'].value], value1=ws1['F3'].value, value2=ws1['F4'].value, trunc=ws1['F6'].value)
     """Initialize an instance of the Insurance class """
     Ins = Insurance(n_o_c,a_p_c)
 
@@ -32,39 +32,57 @@ def main():
     """scaling factor"""
     sc = ws1['F5'].value
 
+    """Bold Font"""
+    b= xl.styles.Font(name='Calibri', size=11, bold=True, color='FF000000')
 
     ws2 = wb.create_sheet(str(Ins))
 
     ws2['A1'].value = "n"
+    ws2['A1'].font = b
+    ws2['A1'].fill = xl.styles.PatternFill("solid", fgColor="009682")
+
     ws2['B1'].value = "P(S=n)"
+    ws2['B1'].font = b
+    ws2['B1'].fill = xl.styles.PatternFill("solid", fgColor="009682")
 
     """write, ruin number expected value and variance"""
-    ws2['K1'].value = "ruin #:"
+    ws2['K1'].value = str(ws1['I2'].value*100)+"% Ruin #"
+    ws2['K1'].font = b
+    ws2.column_dimensions['K'].width = len(str(ws2['K1'].value))
+    ws2['K1'].fill = xl.styles.PatternFill("solid", fgColor="009682")
     ws2['K2'].value = sc * ruin
-    ws2['K3'].value = "("+str(ws1['I2'].value)+")"
 
 
-    ws2['L1'].value = "mean"
+
+    ws2['L1'].value = "Mean"
+    ws2['L1'].font = b
+    ws2['L1'].fill = xl.styles.PatternFill("solid", fgColor="009682")
     ws2['L2'].value = sc*Ins._ex_Value
 
-    ws2['M1'].value = "var"
+    ws2['M1'].value = "Variance"
+    ws2['M1'].font = b
+    ws2['M1'].fill = xl.styles.PatternFill("solid", fgColor="009682")
     ws2['M2'].value = sc**2*Ins._variance
 
     j=0
     while Ins.cantelli(j)>ws1['I2'].value:
         j += 1
 
-    ws2['N1'].value = "cantelli"
+    ws2['N1'].value = "Cantelli"
+    ws2['N1'].font = b
+    ws2['N1'].fill = xl.styles.PatternFill("solid", fgColor="009682")
     ws2['N2'].value = sc*j
 
-    ws2['O1'].value = "sc. factor"
+    ws2['O1'].value = "Sc. Factor"
+    ws2['O1'].font = b
+    ws2['O1'].fill = xl.styles.PatternFill("solid", fgColor="009682")
     ws2['O2'].value = sc
 
     res = 0
     for i in range(0,ruin+1):
         ws2.cell(column=1, row = i+2, value = sc*i)
         ws2.cell( column = 2, row = i+2,value = Ins.prob_S(i))
-        res+= Ins.prob_S(i)
+        res += Ins.prob_S(i)
 
 
 
@@ -77,7 +95,7 @@ def main():
     series = xl.chart.Series(pn, n, title_from_data=True)
 
     chart.series.append(series)
-    chart.title = "Total claim distribution"
+    chart.title = "Total Claims Distribution"
     chart.style = 13
 
     s = chart.series[0]
--- distributions.py
+++ distributions.py
@@ -5,19 +5,19 @@ import math as m
 class Distribution:
     """ Interface class of a Distribution"""
 
-    def __init__(self, type: str, value1: float, value2: float, trunc = False):
+    def __init__(self, type: str, value1: float, value2: float, trunc = 0):
 
         """looks for the distribution type"""
         if type == "B":
-            X = Bin(value1, value2,trunc)
+            X = Bin(value1, value2, trunc)
         elif type == "NB":
-            X = Nbin(value1, value2,trunc)
+            X = Nbin(value1, value2, trunc)
         elif type == "P":
-            X = P(value1, None,trunc)
+            X = P(value1, None, trunc)
         elif type == "LOG":
-            X = Logd(value1, None)
+            X = Logd(value1, None, trunc)
         elif type == "GEO":
-            X = Geo(value1, value2)
+            X = Geo(value1, value2, trunc)
         self._Dist = X
         self._type = X._type
         self._value1 = X._value1
@@ -49,8 +49,9 @@ class Bin:
     _b: float
     _pan: int
     _trunc: int
+    _hdis: Distribution
 
-    def __init__(self, n,p, trunc = False):
+    def __init__(self, n, p, trunc):
         self._type = "B"
         self._value1 = int(n)
         self._value2 = p
@@ -58,26 +59,36 @@ class Bin:
         self._variance = n * p * (1 - p)
         self._a = -p / (1 - p)
         self._b = p*(n+1) / (1 - p)
-        self._pan = 0
-        self._trunc = int(trunc)
-        if trunc:
-            self._pan = 1
+        self._pan = trunc
+        self._trunc = trunc
+        if trunc != 0:
+            self._hdis = Distribution(type=self._type, value1=self._value1, value2=self._value2, trunc=0)
+            x = trunc_sum(trunc,self._hdis)
+            e = self._ex_Value
+            self._ex_Value = (e-x)/(1-x)
+            self._variance = (self._variance+e**2)/(1-x)-self._ex_Value ** 2
+
+
 
     def prob(self, k: int):
         """returns P(X=n)"""
         x=ext_binom(self._value1, k) * self._value2 ** k * (1 - self._value2) ** (self._value1 - k)
-        """1st truncation:"""
-        if self._pan==1:
-            if k==0:
-                return 0
-            x=x/(1- (1 - self._value2) ** (self._value1))
+        """truncation:"""
+        if self._trunc!=0:
+            if k<self._trunc:
+                x =0
+            else:
+                x=x/(1- trunc_sum(n=self._trunc, D=self._hdis))
         return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
         x = (1 - self._value2 + self._value2 * t) ** self._value1
+        """truncation:"""
+        if self._trunc != 0:
+            help = trunc_sum(n=self._trunc, D=self._hdis)
+            x = (x - help) / (1 - help)
         return x
-
 
 class Nbin:
     """ Represents a  negative binomial distribution """
@@ -90,8 +101,9 @@ class Nbin:
     _b: float
     _pan: int
     _trunc: int
+    _hdis: Distribution
 
-    def __init__(self, beta, p, trunc = False):
+    def __init__(self, beta, p, trunc):
         self._type = "NB"
         self._value1 = beta
         self._value2 = p
@@ -99,24 +111,34 @@ class Nbin:
         self._variance = beta * p / (1 - p) ** 2
         self._a = (beta - 1) * p
         self._b = p
-        self._pan = 0
-        self._trunc = int(trunc)
-        if trunc:
-            self._pan = 1
+        self._pan = trunc
+        self._trunc = trunc
+        if trunc != 0:
+            self._hdis = Distribution(type=self._type, value1=self._value1, value2=self._value2, trunc=0)
+            x = trunc_sum(trunc, self._hdis)
+            e = self._ex_Value
+            self._ex_Value = (e - x) / (1 - x)
+            self._variance = (self._variance + e ** 2) / (1 - x) - self._ex_Value ** 2
+
 
     def prob(self, k: int):
         """returns P(X=n)"""
         x=ext_binom(self._value1 + k - 1, k) * self._value2 ** k * (1 - self._value2) ** self._value1
-        """1st truncation:"""
-        if self._pan==1:
-            if k==0:
-                return 0
-            x =x /(1-(1 - self._value2) ** self._value1)
+        """truncation:"""
+        if self._trunc != 0:
+            if k < self._trunc:
+                x = 0
+            else:
+                x = x / (1 - trunc_sum(n=self._trunc, D=self._hdis))
         return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-        x=((1 - self._value2 * t) / (1 - self._value2)) ** self._value1
+        x = ((1 - self._value2 * t) / (1 - self._value2)) ** -self._value1
+        """truncation:"""
+        if self._trunc != 0:
+            help = trunc_sum(n=self._trunc, D=self._hdis)
+            x = (x - help) / (1 - help)
         return x
 
 
@@ -131,8 +153,9 @@ class P:
     _b: float
     _pan: int
     _trunc: int
+    _hdis: Distribution
 
-    def __init__(self, alpha, value2=None, trunc = False):
+    def __init__(self, alpha, trunc, value2=None):
         self._type = "P"
         self._value1 = alpha
         self._value2 = value2
@@ -140,24 +163,34 @@ class P:
         self._variance = alpha
         self._a = 0
         self._b = alpha
-        self._pan = 0
-        self._trunc = int(trunc)
-        if trunc:
-            self._pan = 1
+        self._pan = trunc
+        self._trunc = trunc
+        if trunc != 0:
+            self._hdis = Distribution(type=self._type, value1=self._value1, value2=self._value2, trunc=0)
+            x = trunc_sum(trunc, self._hdis)
+            e = self._ex_Value
+            self._ex_Value = (e - x) / (1 - x)
+            self._variance = (self._variance + e ** 2) / (1 - x) - self._ex_Value ** 2
 
-    def prob(self, k):
+
+    def prob(self, k:int):
         """returns P(X=n)"""
         x =m.exp(-self._value1) * self._value1 ** k / m.factorial(k)
-        """1st truncation:"""
-        if self._pan == 1:
-            if k == 0:
-                return 0
-            x = x / (1 -m.exp(-self._value1))
+        """truncation:"""
+        if self._trunc != 0:
+            if k < self._trunc:
+                x = 0
+            else:
+                x = x / (1 - trunc_sum(n=self._trunc, D=self._hdis))
         return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-        x= m.exp(-self._value1 * (1 - t))
+        x = m.exp(-self._value1 * (1 - t))
+        """truncation:"""
+        if self._trunc != 0:
+            help = trunc_sum(n=self._trunc, D=self._hdis)
+            x = (x - help) / (1 - help)
         return x
 
 
@@ -172,8 +205,9 @@ class Logd:
     _b: float
     _pan: int
     _trunc: int
+    _hdis: Distribution
 
-    def __init__(self, p, value2=None):
+    def __init__(self, p, trunc,value2=None):
         self._type = "LOG"
         self._value1 = p
         self._value2 = value2
@@ -181,19 +215,37 @@ class Logd:
         self._variance = (-m.log(1 - p) - p) / (m.log(1 - p)) ** 2 * p / (1 - p) ** 2
         self._a = p
         self._b = -p
-        self._pan = 1
-        self._trunc = 0
+        self._pan = max([1, trunc])
+        self._trunc = trunc
+        if trunc != 0:
+            self._hdis = Distribution(type=self._type, value1=self._value1, value2=self._value2, trunc=0)
+            x = trunc_sum(trunc, self._hdis)
+            e = self._ex_Value
+            self._ex_Value = (e - x) / (1 - x)
+            self._variance = (self._variance + e ** 2) / (1 - x) - self._ex_Value ** 2
 
 
     def prob(self, k: int):
         """returns P(X=n)"""
         if k == 0:
             return 0
-        return -1 / m.log(1 - self._value1) * self._value1 ** k / k
+        x = -1 / m.log(1 - self._value1) * self._value1 ** k / k
+        """truncation:"""
+        if self._trunc != 0:
+            if k < self._trunc:
+                x = 0
+            else:
+                x = x / (1 - trunc_sum(n=self._trunc, D=self._hdis))
+        return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-        return m.log(1 - self._value1 * t) / m.log(1 - self._value1)
+        x = m.log(1 - self._value1 * t) / m.log(1 - self._value1)
+        """truncation:"""
+        if self._trunc != 0:
+            help = trunc_sum(n=self._trunc, D=self._hdis)
+            x = (x - help) / (1 - help)
+        return x
 
 
 class Geo:
@@ -207,8 +259,9 @@ class Geo:
     _b: float
     _pan: int
     _trunc: int
+    _hdis: Distribution
 
-    def __init__(self, n, p):
+    def __init__(self, n, p, trunc):
         self._type = "GEO"
         self._value1 = n
         self._value2 = p
@@ -216,22 +269,38 @@ class Geo:
         self._variance = n * (1 - p) / p ** 2
         self._a = None
         self._b = None
-        self._trunc = 0
-        self._pan = 1
+        self._trunc = trunc
+        self._pan = max([1, trunc])
+        if trunc != 0:
+            self._hdis = Distribution(type=self._type, value1=self._value1, value2=self._value2, trunc=0)
+            x = trunc_sum(trunc, self._hdis)
+            e = self._ex_Value
+            self._ex_Value = (e - x) / (1 - x)
+            self._variance = (self._variance + e ** 2) / (1 - x) - self._ex_Value ** 2
 
 
     def prob(self, k):
         """returns P(X=n)"""
         if k == 0:
             return 0
-        return ext_binom(k - 1, self._value1 - 1) * self._value2 ** self._value1 * ((1 - self._value2) ** (
+        x=ext_binom(k - 1, self._value1 - 1) * self._value2 ** self._value1 * ((1 - self._value2) ** (
                 k - self._value1))
+        """truncation:"""
+        if self._trunc != 0:
+            if k < self._trunc:
+                x = 0
+            else:
+                x = x / (1 - trunc_sum(n=self._trunc, D=self._hdis))
+        return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
 
         x = ((self._value2 * t) / (1 - (1 - self._value2) * t)) ** self._value1
-
+        """truncation:"""
+        if self._trunc != 0:
+            help = trunc_sum(n=self._trunc, D=self._hdis)
+            x = (x-help) / (1 - help)
         return x
 
 
@@ -240,4 +309,15 @@ def ext_binom(n: float, k: int):
     res = 1
     for i in range(1, k+1):
         res *= (n+1-i)/i
+    return res
+
+def trunc_sum(n:int,D:Distribution):
+    """calculates the sum for truncation
+       n: n-truncation
+       X: Distribution without truncation
+    """
+    res = 0
+    if n !=0:
+        for i in range(n):
+            res += D.prob(i)
     return res
