8cf7701da9643c677108b6711554de32baf04a05
XaverWangerpohl <71377493+XaverWangerpohl@users.noreply.github.com>
2021-03-23 18:10:54 +0100

    Add files via upload

--- distributions.py
+++ distributions.py
@@ -1,243 +1,382 @@
-
 import math as m
 
 
 class Distribution:
     """ Interface class of a Distribution"""
-
-    def __init__(self, type: str, value1: float, value2: float, trunc = False):
+    _type: str
+    _value1: int
+    _value2: float
+    _ex_value: float
+    _variance: float
+    _a: float
+    _b: float
+    _pan: int
+    _trunc: int
+    def __init__(self, typ: str, value1: float, value2: float, trunc: int):
 
         """looks for the distribution type"""
-        if type == "B":
-            X = Bin(value1, value2,trunc)
-        elif type == "NB":
-            X = Nbin(value1, value2,trunc)
-        elif type == "P":
-            X = P(value1, None,trunc)
-        elif type == "LOG":
-            X = Logd(value1, None)
-        elif type == "GEO":
-            X = Geo(value1, value2)
-        self._Dist = X
-        self._type = X._type
-        self._value1 = X._value1
-        self._value2 = X._value2
-        self._ex_Value = X._ex_Value
-        self._variance = X._variance
-        self._a = X._a
-        self._b = X._b
-        self._pan = X._pan
-        self._trunc = X._trunc
+        if typ == "B":
+            dist = Bin(value1, value2, trunc)
+        elif typ == "NB":
+            dist = NBin(value1, value2, trunc)
+        elif typ == "P":
+            dist = P(value1, trunc)
+        elif typ == "LOG":
+            dist = Log(value1, trunc)
+        elif typ == "ENB":
+            dist = ENB(value1, value2, trunc)
+        """set the attributes accordingly"""
+        self._dist = dist
+        self._typ = dist.typ
+        self._value1 = dist.value1
+        self._value2 = dist.value2
+        self._ex_value = dist.ex_value
+        self._variance = dist.variance
+        self._a = dist.a
+        self._b = dist.b
+        self._pan = dist.pan
+        self._trunc = dist.trunc
+    """getter methods for the attributes and methods"""
 
+    def get_typ(self):
+        return self._typ
 
-    def prob(self, k: int):
-        return self._Dist.prob(k)
+    def get_value1(self):
+        return self._value1
 
-    def pgf(self, t: float):
+    def get_value2(self):
+        return self._value2
+
+    def get_ex_value(self):
+        return self._ex_value
+
+    def get_variance(self):
+        return self._variance
+
+    def get_a(self):
+        return self._a
+
+    def get_b(self):
+        return self._b
+
+    def get_pan(self):
+        return self._pan
+
+    def get_trunc(self):
+        return self._trunc
+
+    def get_prob(self, k: int):
+        return self._dist.prob(k)
+
+    def get_pgf(self, t: float):
         """returns m_X(t)"""
-        return self._Dist.pgf(t)
+        return self._dist.pgf(t)
+
+
+class TruncHelper:
+    """This class provides the framework for distributions to be truncated."""
+    trunc_sums: list
+    probabilities: list
+    trunc: int
+
+    def __init__(self, typ: str, value1: float, value2: float, trunc: int):
+        d = Distribution(typ=typ, value1=value1, value2=value2, trunc=0)
+        """_probabilities saves the probabilities of the non truncated distribution up until trunc-1"""
+        self.probabilities = []
+        for i in range(trunc):
+            self.probabilities.append(d.get_prob(i))
+        """"_trunc sums saves the values of the sums returned by set_trunc_sums"""
+        self.trunc_sums = set_trunc_sums(trunc, d)
+        self.trunc = trunc
+
+    def set_ex_v_var(self, ex_value: float, variance: float):
+        """returns expected value and variance of the truncated distribution
+        with the formulas of the thesis((2.8)and(2.9))"""
+        new_ex_value = (ex_value - self.trunc_sums[1]) / (1 - self.trunc_sums[0])
+        new_variance = (variance + (ex_value ** 2) - self.trunc_sums[2]) / (1 - self.trunc_sums[0]) - new_ex_value ** 2
+        return [new_ex_value, new_variance]
+
+    def set_prob(self, x: float, k: int):
+        """returns P(Xtrunc=k) of the truncated random variable, where x = P(X=k) """
+        if self.trunc > k:
+            return 0
+        return x / (1 - self.trunc_sums[0])
+
+    def set_pgf(self, x: float, t: float):
+        """analogous to set_prob but with the pgf"""
+        tr_sum = 0
+        for i in range(len(self.probabilities)):
+            tr_sum += self.probabilities[i] * t ** i
+
+        return (x - tr_sum) / (1 - self.trunc_sums[0])
+
+
+"""The possible distributions are implemented here"""
 
 
 class Bin:
     """ Represents a binomial distribution """
-    _type: str
-    _value1: int
-    _value2: float
-    _ex_Value: float
-    _variance: float
-    _a: float
-    _b: float
-    _pan: int
-    _trunc: int
+    typ: str
+    value1: int
+    value2: float
+    ex_value: float
+    variance: float
+    a: float
+    b: float
+    pan: int
+    trunc: int
+    tr_help: TruncHelper
 
-    def __init__(self, n,p, trunc = False):
-        self._type = "B"
-        self._value1 = int(n)
-        self._value2 = p
-        self._ex_Value = n * p
-        self._variance = n * p * (1 - p)
-        self._a = -p / (1 - p)
-        self._b = p*(n+1) / (1 - p)
-        self._pan = 0
-        self._trunc = int(trunc)
-        if trunc:
-            self._pan = 1
+    def __init__(self, n, p, trunc):
+        self.typ = "B"
+        self.value1 = int(n)
+        self.value2 = p
+        self.ex_value = n * p
+        self.variance = n * p * (1 - p)
+        self.a = -p / (1 - p)
+        self.b = p * (n + 1) / (1 - p)
+        self.pan = trunc
+        self.trunc = trunc
+        """change expected value and variance, if the distribution is truncated"""
+        if trunc != 0:
+            self.tr_help = TruncHelper(typ=self.typ, value1=self.value1, value2=self.value2, trunc=trunc)
+            """set new expected value and variance"""
+            ex_val_and_v = self.tr_help.set_ex_v_var(ex_value=self.ex_value, variance=self.variance)
+            self.ex_value = ex_val_and_v[0]
+            self.variance = ex_val_and_v[1]
 
     def prob(self, k: int):
         """returns P(X=n)"""
-        x=ext_binom(self._value1, k) * self._value2 ** k * (1 - self._value2) ** (self._value1 - k)
-        """1st truncation:"""
-        if self._pan==1:
-            if k==0:
-                return 0
-            x=x/(1- (1 - self._value2) ** (self._value1))
+        x = ext_binom(self.value1, k) * self.value2 ** k * (1 - self.value2) ** (self.value1 - k)
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_prob(x, k)
         return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-        x = (1 - self._value2 + self._value2 * t) ** self._value1
+        x = (1 - self.value2 + self.value2 * t) ** self.value1
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_pgf(x, t)
         return x
 
 
-class Nbin:
+class NBin:
     """ Represents a  negative binomial distribution """
-    _type: str
-    _value1: float
-    _value2: float
-    _ex_Value: float
-    _variance: float
-    _a: float
-    _b: float
-    _pan: int
-    _trunc: int
+    typ: str
+    value1: float
+    value2: float
+    ex_value: float
+    variance: float
+    a: float
+    b: float
+    pan: int
+    trunc: int
+    tr_help: TruncHelper
 
-    def __init__(self, beta, p, trunc = False):
-        self._type = "NB"
-        self._value1 = beta
-        self._value2 = p
-        self._ex_Value = beta * p / (1 - p)
-        self._variance = beta * p / (1 - p) ** 2
-        self._a = (beta - 1) * p
-        self._b = p
-        self._pan = 0
-        self._trunc = int(trunc)
-        if trunc:
-            self._pan = 1
+    def __init__(self, beta, p, trunc):
+        self.typ = "NB"
+        self.value1 = beta
+        self.value2 = p
+        self.ex_value = beta * p / (1 - p)
+        self.variance = beta * p / (1 - p) ** 2
+        self.a = p
+        self.b = (beta - 1) * p
+        self.pan = trunc
+        self.trunc = trunc
+        """change expected value and variance, if the distribution is truncated"""
+        if trunc != 0:
+            self.tr_help = TruncHelper(typ=self.typ, value1=self.value1, value2=self.value2, trunc=trunc)
+            """set new expected value and variance"""
+            ex_val_and_v = self.tr_help.set_ex_v_var(ex_value=self.ex_value, variance=self.variance)
+            self.ex_value = ex_val_and_v[0]
+            self.variance = ex_val_and_v[1]
 
     def prob(self, k: int):
         """returns P(X=n)"""
-        x=ext_binom(self._value1 + k - 1, k) * self._value2 ** k * (1 - self._value2) ** self._value1
-        """1st truncation:"""
-        if self._pan==1:
-            if k==0:
-                return 0
-            x =x /(1-(1 - self._value2) ** self._value1)
+        x = ext_binom(self.value1 + k - 1, k) * self.value2 ** k * (1 - self.value2) ** self.value1
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_prob(x, k)
         return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-        x = ((1 - self._value2 * t) / (1 - self._value2)) ** -self._value1
+        x = ((1 - self.value2 * t) / (1 - self.value2)) ** -self.value1
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_pgf(x, t)
         return x
 
 
 class P:
     """ Represents a  Poisson distribution """
-    _type: str
-    _value1: float
-    _value2: None
-    _ex_Value: float
-    _variance: float
-    _a: float
-    _b: float
-    _pan: int
-    _trunc: int
+    typ: str
+    value1: float
+    value2: None
+    ex_value: float
+    variance: float
+    a: float
+    b: float
+    pan: int
+    trunc: int
+    tr_help: TruncHelper
 
-    def __init__(self, alpha, value2=None, trunc = False):
-        self._type = "P"
-        self._value1 = alpha
-        self._value2 = value2
-        self._ex_Value = alpha
-        self._variance = alpha
-        self._a = 0
-        self._b = alpha
-        self._pan = 0
-        self._trunc = int(trunc)
-        if trunc:
-            self._pan = 1
+    def __init__(self, alpha, trunc):
+        self.typ = "P"
+        self.value1 = alpha
+        self.value2 = None
+        self.ex_value = alpha
+        self.variance = alpha
+        self.a = 0
+        self.b = alpha
+        self.pan = trunc
+        self.trunc = trunc
+        """change expected value and variance, if the distribution is truncated"""
+        if trunc != 0:
+            self.tr_help = TruncHelper(typ=self.typ, value1=self.value1, value2=self.value2, trunc=trunc)
+            """set new expected value and variance"""
+            ex_val_and_v = self.tr_help.set_ex_v_var(ex_value=self.ex_value, variance=self.variance)
+            self.ex_value = ex_val_and_v[0]
+            self.variance = ex_val_and_v[1]
 
-    def prob(self, k):
+    def prob(self, k: int):
         """returns P(X=n)"""
-        x =m.exp(-self._value1) * self._value1 ** k / m.factorial(k)
-        """1st truncation:"""
-        if self._pan == 1:
-            if k == 0:
-                return 0
-            x = x / (1 -m.exp(-self._value1))
+        x = m.exp(-self.value1) * self.value1 ** k / m.factorial(k)
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_prob(x, k)
         return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-        x= m.exp(-self._value1 * (1 - t))
+        x = m.exp(-self.value1 * (1 - t))
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_pgf(x, t)
         return x
 
 
-class Logd:
+class Log:
     """ Represents a logarithmic distribution """
-    _type: str
-    _value1: float
-    _value2: None
-    _ex_Value: float
-    _variance: float
-    _a: float
-    _b: float
-    _pan: int
-    _trunc: int
+    typ: str
+    value1: float
+    value2: None
+    ex_value: float
+    variance: float
+    a: float
+    b: float
+    pan: int
+    trunc: int
+    tr_help: TruncHelper
 
-    def __init__(self, p, value2=None):
-        self._type = "LOG"
-        self._value1 = p
-        self._value2 = value2
-        self._ex_Value = -1 * m.log(1 - p) * p / (1 - p)
-        self._variance = (-m.log(1 - p) - p) / (m.log(1 - p)) ** 2 * p / (1 - p) ** 2
-        self._a = p
-        self._b = -p
-        self._pan = 1
-        self._trunc = 0
-
+    def __init__(self, p, trunc):
+        self.typ = "LOG"
+        self.value1 = p
+        self.value2 = None
+        self.ex_value = -1 / m.log(1 - p) * p / (1 - p)
+        self.variance = (-m.log(1 - p) - p) / ((m.log(1 - p)) ** 2) * p / (1 - p) ** 2
+        self.a = p
+        self.b = -p
+        self.pan = max([1, trunc])
+        self.trunc = trunc
+        """change expected value and variance, if the distribution is truncated"""
+        if trunc != 0:
+            self.tr_help = TruncHelper(typ=self.typ, value1=self.value1, value2=self.value2, trunc=trunc)
+            """set new expected value and variance"""
+            ex_val_and_v = self.tr_help.set_ex_v_var(ex_value=self.ex_value, variance=self.variance)
+            self.ex_value = ex_val_and_v[0]
+            self.variance = ex_val_and_v[1]
 
     def prob(self, k: int):
         """returns P(X=n)"""
         if k == 0:
             return 0
-        return -1 / m.log(1 - self._value1) * self._value1 ** k / k
+        x = -1 / m.log(1 - self.value1) * self.value1 ** k / k
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_prob(x, k)
+        return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-        return m.log(1 - self._value1 * t) / m.log(1 - self._value1)
+        x = m.log(1 - self.value1 * t) / m.log(1 - self.value1)
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_pgf(x, t)
+        return x
 
 
-class Geo:
-    """ Represents a geometric distribution """
-    _type: str
-    _value1: int
-    _value2: float
-    _ex_Value: float
-    _variance: float
-    _a: float
-    _b: float
-    _pan: int
-    _trunc: int
+class ENB:
+    """ Represents a extended negative binomial distribution with n=1 """
+    typ: str
+    value1: int
+    value2: float
+    ex_value: float
+    variance: float
+    a: float
+    b: float
+    pan: int
+    trunc: int
+    tr_help: TruncHelper
 
-    def __init__(self, n, p):
-        self._type = "GEO"
-        self._value1 = n
-        self._value2 = p
-        self._ex_Value = n / p
-        self._variance = n * (1 - p) / p ** 2
-        self._a = None
-        self._b = None
-        self._trunc = 0
-        self._pan = 1
-
+    def __init__(self, beta, p, trunc):
+        self.typ = "ENB"
+        self.value1 = beta
+        self.value2 = p
+        """calculates the expected value and variance of the non truncated distribution"""
+        self.ex_value = (beta * p * (1 - p) ** (-beta - 1)) / ((1 - p) ** (-beta) - 1)
+        self.variance = (beta * (beta + 1) * p * p * (1 - p) ** (-beta - 2)) / (
+                (1 - p) ** (-beta) - 1) + self.ex_value - self.ex_value ** 2
+        self.a = p
+        self.b = (beta - 1) * p
+        self.trunc = trunc
+        self.pan = max([1, trunc])
+        """change expected value and variance, if the distribution is truncated"""
+        if trunc != 0:
+            self.tr_help = TruncHelper(typ=self.typ, value1=self.value1, value2=self.value2, trunc=trunc)
+            """set new expected value and variance"""
+            ex_val_and_v = self.tr_help.set_ex_v_var(ex_value=self.ex_value, variance=self.variance)
+            self.ex_value = ex_val_and_v[0]
+            self.variance = ex_val_and_v[1]
 
     def prob(self, k):
         """returns P(X=n)"""
         if k == 0:
             return 0
-        return ext_binom(k - 1, self._value1 - 1) * self._value2 ** self._value1 * ((1 - self._value2) ** (
-                k - self._value1))
+        x = ext_binom(self.value1 + k - 1, k) * self.value2 ** k / ((1 - self.value2) ** (-self.value1) - 1)
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_prob(x, k)
+        return x
 
     def pgf(self, t: float):
         """returns m_X(t)"""
-
-        x = ((self._value2 * t) / (1 - (1 - self._value2) * t)) ** self._value1
-
+        x = ((1 - self.value2 * t) ** (-self.value1) - 1) / ((1 - self.value2) ** (-self.value1) - 1)
+        """truncation:"""
+        if self.trunc != 0:
+            x = self.tr_help.set_pgf(x, t)
         return x
 
 
 def ext_binom(n: float, k: int):
     """ calculates real valued binomial coefficient"""
     res = 1
-    for i in range(1, k+1):
-        res *= (n+1-i)/i
+    for i in range(1, k + 1):
+        res *= (n + 1 - i) / i
+    return res
+
+
+def set_trunc_sums(n: int, d: Distribution):
+    """calculates the sums for truncation (probability, expected value and variance)
+       n: n-truncation
+       X: Distribution without truncation
+    """
+    res = [0, 0, 0]
+    if n != 0:
+        for i in range(n):
+            res[0] += d.get_prob(i)
+            res[1] += i * d.get_prob(i)
+            res[2] += (i ** 2) * d.get_prob(i)
     return res
--- insurance.py
+++ insurance.py
@@ -1,79 +1,95 @@
 from distributions import Distribution
 
+
 class Insurance:
     """This class models an insurance following a collective model and is a framework for calculating properties of
     the total damage claim distribution"""
-    _N:Distribution
-    _X:Distribution
+    _N: Distribution
+    _X: Distribution
     """expected value of S"""
-    _ex_Value:float
+    _ex_value: float
     """variance of S"""
-    _variance:float
-    g:list
+    _variance: float
+    _g: list
 
-
-    def __init__(self,n_o_c:Distribution,a_p_c:Distribution):
+    def __init__(self, n_o_c: Distribution, a_p_c: Distribution):
         self._N = n_o_c
         self._X = a_p_c
         """compare Wald's equations"""
-        self._ex_Value = n_o_c._ex_Value*a_p_c._ex_Value
-        self._variance = n_o_c._ex_Value*a_p_c._variance+n_o_c._variance*a_p_c._ex_Value ** 2
-        self.g = [self._N.pgf(self._X.prob(0))]
-        if self._N._pan == 1 :
-            self.g[0] = 0
-            self.g.append(self._N.prob(1)*self._X.prob(1))
+        self._ex_value = n_o_c.get_ex_value() * a_p_c.get_ex_value()
+        self._variance = n_o_c.get_ex_value() * a_p_c.get_variance() + n_o_c.get_variance() * a_p_c.get_ex_value() ** 2
+        self._g = [self._N.get_pgf(self._X.get_prob(0))]
+        if self._N.get_pan() == 1:
+            self._g[0] = 0
+            self._g.append(self._N.get_prob(1) * self._X.get_prob(1))
 
     def __str__(self):
-        T=""
-        B =""
-        if self._N._trunc ==1:
-            T="tr"
-        if self._X._trunc == 1:
-            B="tr"
-        if self._N._value2 == None:
-            N = self._N._type+"("+str(self._N._value1)+")"
+        t = ""
+        b = ""
+        if self._N.get_trunc() == 1:
+            t = "tr"
+        if self._X.get_trunc() == 1:
+            b = "tr"
+        if self._N.get_value2() is None:
+            n = self._N.get_typ() + "(" + str(self._N.get_value1()) + ")"
         else:
-            N = self._N._type+"("+str(self._N._value1)+","+str(self._N._value2)+")"
+            n = self._N.get_typ() + "(" + str(self._N.get_value1()) + "," + str(self._N.get_value2()) + ")"
 
-        if self._X._value2 == None:
-            X = self._X._type+"("+str(self._X._value1)+")"
+        if self._X.get_value2() is None:
+            x = self._X.get_typ() + "(" + str(self._X.get_value1()) + ")"
         else:
-            X = self._X._type+"("+str(self._X._value1)+","+str(self._X._value2)+")"
-        return N+T+X+B
+            x = self._X.get_typ() + "(" + str(self._X.get_value1()) + "," + str(self._X.get_value2()) + ")"
+        return n + t + x + b
 
-    def ruin(self,p: float):
+    def get_premium(self, p: float):
         """returns the minimal income of the insurance to achieve a probability of ruin less than p"""
-        G = self.g[0]
+        prob_sum = self._g[0]
         i = 0
-        while G <= (1-p):
+        while prob_sum <= (1 - p):
             i += 1
-            G += self.prob_S(i)
+            prob_sum += self.get_prob_s(i)
         return i
 
-
-    def prob_S(self, k:int):
+    def get_prob_s(self, k: int):
         """returns P(S=k)"""
-        n = len(self.g)
-        if n-1 >=k:
-            return self.g[k]
-        while n<=k:
-            if self._N._pan == 0:
-                self.g.append(self.helpsum(n)/(1-self._N._a*self._X.prob(0)))
+        n = len(self._g)
+        """cheks if the probability has already been calculated"""
+        if n - 1 >= k:
+            return self._g[k]
+        """panjer recursion"""
+        while n <= k:
+            """case N in Pan(0)"""
+            if self._N.get_pan() == 0:
+                self._g.append(self.helpsum(n) / (1 - self._N.get_a() * self._X.get_prob(0)))
             else:
-                self.g.append((self.helpsum(n)+self._N.prob(1)*self._X.prob(n)) / (1 - self._N._a * self._X.prob(0)))
-            n = n+1
+                """case N in Pan(1)"""
+                self._g.append(
+                    (self.helpsum(n) + self._N.get_prob(1) * self._X.get_prob(n)) / (1 - self._N.get_a() * self._X.get_prob(0)))
+            n = n + 1
 
-        return self.g[k]
+        return self._g[k]
+    """getter metods"""
+    def get_ex_value(self):
+        return self._ex_value
 
-    def cantelli(self,c:float):
+    def get_variance(self):
+        return self._variance
+
+    def get_cantelli_prob(self, c: float):
         """ returns Cantelli Inequation for P(S<=E(S)+c)"""
-        return self._variance/(c**2 + self._variance)
+        return self._variance / (c ** 2 + self._variance)
 
+    def get_cantelli_premium(self, p: float):
+        """returns the minimal premium, such that the probability
+        of ruin is smaller than p by cantelli's inequality  """
+        """solve cantelli inequality for c"""
+        c = ((self._variance/p)-self._variance)**(1/2)
+        return self._ex_value+c
 
     """for the calculation of prob_S"""
-    def helpsum(self, n:int):
-        r = 0
-        for k in range(1,n+1):
-            r += (self._N._a + self._N._b*k/n)*self.g[n-k]*self._X.prob(k)
-        return r
 
+    def helpsum(self, n: int):
+        r = 0
+        for k in range(1, n + 1):
+            r += (self._N.get_a() + self._N.get_b() * k / n) * self._g[n - k] * self._X.get_prob(k)
+        return r
